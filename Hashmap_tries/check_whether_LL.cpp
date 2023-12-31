a879490bf992c234b2cfb588cd70fa803e39241a
sumit rai <RAIS74184@gmail.com>
2023-12-31 14:41:24 +0530

    adding new values

--- Hashmap_tries/check_whether_LL.cpp
+++ Hashmap_tries/check_whether_LL.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 //to check whether the following is the circular linked_list 
 
-
+//make a node --table with 
+//mark all the value as true once that pointer arrives on that value again mark it as repeitition
 int main(){
 
 
