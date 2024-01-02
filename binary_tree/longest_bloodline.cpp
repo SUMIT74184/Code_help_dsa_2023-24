fcae608dbade5d0af6cbb9395318e83f75ea7df9
sumit rai <RAIS74184@gmail.com>
2024-01-02 19:18:38 +0530

    updated_long

---
+++ binary_tree/longest_bloodline.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+int data;
+Node*left;
+Node*right;
+
+Node(int val){
+    this->data=val;
+    this->left=NULL;
+    this->right=NULL;
+}
+};
+
+void longestHeight(Node*){
+    
+}
+
+int main(){
+
+    return 0;
+}
